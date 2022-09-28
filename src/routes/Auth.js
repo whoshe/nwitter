e85f37f22d065c48adf71d722e8d0bf69687bac8
Changwoo Nam <whoshe@funnyplan.com>
2022-09-29 08:59:24 +0900

    auth

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default () => <span>인증 페이지 </span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          name="email"
+          value={email}
+          type="text"
+          placeholder="Email"
+          required
+        />
+        <input
+          onChange={onChange}
+          name="password"
+          value={password}
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <input type="submit" value="Log in" />
+      </form>
+      <button>Continue with Google</button>
+      <button>Continue with Github</button>
+    </div>
+  );
+};
+export default Auth;
